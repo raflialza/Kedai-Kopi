63191d187caf2fc9aedff9687e194829bd179f3f
raflialza <muhammadraflialza@gmail.com>
2025-08-18 19:37:21 +0700

    update logika hamburger

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,15 @@
-// Toggle class active
+// Toggle the 'active' class on the navigation menu
 const navbarNav = document.querySelector(".navbar-nav");
-// ketika hamburger menu diklik
-const hamburger = (document.querySelector("#hamburger-menu").onclick = () => {
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+// When the hamburger menu is clicked
+hamburgerMenu.addEventListener("click", (event) => {
+  // Prevent the default behavior of the anchor tag
+  event.preventDefault();
   navbarNav.classList.toggle("active");
 });
 
-// Klik di luar sidebar untuk menghilangkan sidebar
-const hamburgerMenu = document.querySelector("#hamburger-menu");
-
+// Close the navigation menu when clicking outside of it
 document.addEventListener("click", function (e) {
   if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
